50fff98c5d1deb4d50cf475f29c66dd38769320d
Manuel Hurtado <podfinkx@protonmail.com>
2020-10-07 15:37:29 -0400

    :bug: Fix next-seo.config location

--- site/src/pages/_app.js
+++ site/src/pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 import { DefaultSeo } from 'next-seo';
 
 // import your default seo configuration
-import SEO from '../next-seo.config';
+import SEO from '../../next-seo.config';
 
 export default function MyApp({ Component, pageProps }) {
   return (

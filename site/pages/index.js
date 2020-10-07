e1c7fc2a029c5e7bbe28d5f0bfdecc179a9fc88f
Manuel Hurtado <podfinkx@protonmail.com>
2020-10-07 15:29:47 -0400

    :wrench: Add babelrc and babel-plugin-macros :construction: Create temporal landing page

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,30 +1,49 @@
+import React from 'react';
 import Head from 'next/head';
-import Layout from '../components/Layout';
-import PostList from '../components/PostList';
+import tw from 'twin.macro';
+import styled from '@emotion/styled';
 
-export default function Index({ posts }) {
+// Styled Components
+const StyledIndex = styled.section`
+  div {
+    ${tw`w-full h-auto min-h-screen max-h-screen`}
+    main {
+      ${tw`bg-blue-800`};
+      div {
+        ${tw`flex flex-col items-center justify-center m-auto w-3/4`};
+        h1 {
+          ${tw`text-blue-300 text-3xl text-center`};
+        }
+        p {
+          ${tw`text-gray-400 text-xl text-center`};
+        }
+      }
+    }
+  }
+`;
+
+const Index = () => {
   return (
-    <Layout>
-      <PostList posts={posts} />
-    </Layout>
+    <StyledIndex>
+      <div>
+        <Head>
+          <title>0x81d</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+          <div className="banner">
+            <h1>Welcome to My Website 🚧(WIP)</h1>
+            <p>I am learning by doing so...</p>
+
+            <wbr />
+
+            <p>I'm working on the design of this website ⏳</p>
+          </div>
+        </main>
+      </div>
+    </StyledIndex>
   );
-}
+};
 
-export async function getStaticProps() {
-  // Create an instance of the Contentful JavaScript SDK
-  const client = require('contentful').createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-  });
-
-  // Fetch all entries of content_type `blogPost`
-  const posts = await client
-    .getEntries({ content_type: 'blogPost' })
-    .then((response) => response.items);
-
-  return {
-    props: {
-      posts,
-    },
-  };
-}
+export default Index;
